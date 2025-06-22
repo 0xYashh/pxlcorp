feeb70f7c16d5e5585c65b01ae4e961dd2c43e36
Yash Kumar <yashkr044@gmail.com>
2025-06-22 23:22:49 +0530

    fix: config error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-
-
-module.exports = nextConfig /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
