e23951db93290d71c77e06b013bf71eab3f74ee2
Yash Kumar <yashkr044@gmail.com>
2025-06-22 23:11:53 +0530

    Add page metadata & clean project

--- app/page.js
+++ app/page.js
@@ -5,6 +5,11 @@ import Reads from '../components/Reads'
 import Team from '../components/Team'
 import Footer from '../components/Footer'
 
+export const metadata = {
+  title: 'pxlcorp',
+  description: 'tiny internet lab building weird things on the internet',
+}
+
 export default function Home() {
   return (
     <main id="main-content">
